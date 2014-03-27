d37453984b9628f013f4cbf1efe2ca3bc8146a1f
Scott <scott@dottedt.com>
2014-03-27 11:17:34 -0500

    adding a controller

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,6 @@
+class QuotesController < DottedRulers::QuotesController
+  def a_quote
+    "Hell hath no furrey like a cat" +
+      "shooed off your keyboard"
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,9 @@
 require "dotted_rulers"
 
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+
+require "quotes_controller"
+
 module BestQuotes
   class Application < DottedRulers::Application
   end
