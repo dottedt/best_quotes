f53c28b6195a16a4e384900c2615cf6430c41d7c
Scott <scott@dottedt.com>
2014-03-28 18:34:55 -0500

    Typo correction

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,4 @@
-class QuotesController < DottedRulers::QuotesController
+class QuotesController < DottedRulers::Controller
   def a_quote
     "Hell hath no furrey like a cat" +
       "shooed off your keyboard"
