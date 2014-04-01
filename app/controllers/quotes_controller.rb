e70f6853145a31ae84a3b7968ca18bfdd07634e6
Scott <scott@dottedt.com>
2014-03-31 22:39:47 -0500

    adding exception

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,11 @@
 class QuotesController < DottedRulers::Controller
   def a_quote
     "Hell hath no furrey like a cat" +
-      "shooed off your keyboard"
+      "shooed off your keyboard" +
+      "\n<pre>\n#{env}\n</pre>"
+  end
+
+  def exception
+    raise "Holy flocking snitz this is bad!"
   end
 end
